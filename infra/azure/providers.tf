83cfc281584321fe17cfbd9a1c4fb2f174fbfbc8
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-06 22:19:04 +0530

    helm and k8s provider added to AKS Cluster

--- infra/azure/providers.tf
+++ infra/azure/providers.tf
@@ -4,10 +4,35 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=4.1.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 3.0"
+    }
   }
 }
 
 provider "azurerm" {
   resource_provider_registrations = "none"
   features {}
+  use_cli = true
+}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.demo.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = azurerm_kubernetes_cluster.demo.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.demo.kube_config.0.cluster_ca_certificate)
+  }
 }
