e175f9757f31dc9de9d1df6413d4ee3f2ded43f0
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-06 22:19:30 +0530

    Fluent-bit user keys storing in k8s secret

--- infra/azure/security.tf
+++ infra/azure/security.tf
@@ -8,3 +8,16 @@ resource "azurerm_role_assignment" "demo-2" {
   role_definition_name = "Reader"
   principal_id         = azurerm_kubernetes_cluster.demo.identity[0].principal_id
 }
+
+# Kubernetes Secret for storing credentials
+resource "kubernetes_secret" "fluentbit_aws_creds" {
+  metadata {
+    name      = "fluentbit-aws-credentials" 
+  }
+  type = "Opaque"
+  data = {
+    AWS_ACCESS_KEY_ID     = local.fluentbit_access_key
+    AWS_SECRET_ACCESS_KEY = local.fluentbit_secret_key
+    AWS_REGION = "ap-south-1"
+  }
+}
