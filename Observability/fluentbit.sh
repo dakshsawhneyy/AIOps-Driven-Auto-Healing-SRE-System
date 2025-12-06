292abdc84a8fece67a0c63a0e197ed1b4099b9a5
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-06 10:09:39 +0530

    fluentbut install cmd and its values.yml introduced

---
+++ Observability/fluentbit.sh
@@ -0,0 +1,9 @@
+# Add the Fluent Helm repository:
+helm repo add fluent https://fluent.github.io/helm-charts
+helm repo update
+
+# Install Fluent Bit in a dedicated namespace (e.g., fluent-bit):
+helm upgrade --install fluent-bit fluent/fluent-bit --namespace fluent-bit --create-namespace -f values.yaml
+
+# Verify pods
+kubectl get pods -n fluent-bit -o wide
