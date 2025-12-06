f93fff36a2ab359b299611d2792f6597792dcb4d
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-06 19:25:17 +0530

    Fluent Bit Install script added

--- Observability/fluentbit.sh
+++ Observability/fluentbit.sh
@@ -3,7 +3,8 @@ helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
 
 # Install Fluent Bit in a dedicated namespace (e.g., fluent-bit):
-helm upgrade --install fluent-bit fluent/fluent-bit --namespace fluent-bit --create-namespace -f values.yaml
+helm upgrade --install fluent-bit fluent/fluent-bit -f fluent-bit.conf
+# helm upgrade --install fluent-bit fluent/fluent-bit --namespace fluent-bit --create-namespace -f values.yaml
 
 # Verify pods
 kubectl get pods -n fluent-bit -o wide
