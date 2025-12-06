1f6e482a6d167729ec253e149f6112585ccfe47b
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-06 19:05:01 +0530

    Outputs specified in AWS Stack

--- infra/aws/outputs.tf
+++ infra/aws/outputs.tf
@@ -6,19 +6,13 @@ output "kinesis_stream_name" {
   value = aws_kinesis_stream.fluentbit_metrics.name
 }
 
-output "fluentbit_role_arn" {
-  value = aws_iam_role.fluentbit_role.arn
-}
-
 
 # IAM User Access Keys
 output "fluentbit_access_key" {
-  value = aws_iam_access_key.fluentbit.id
+  value = aws_iam_access_key.fluentbit_keys.id
   sensitive = true
 }
 output "fluentbit_secret_key" {
-  value = aws_iam_access_key.fluentbit.secret
+  value = aws_iam_access_key.fluentbit_keys.secret
   sensitive = true
 }
-
-
