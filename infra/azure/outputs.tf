e81602700140af721f813c789d98ffb60b86ebcb
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-06 22:16:54 +0530

    AKS Outputs is shown now in azure stack

--- infra/azure/outputs.tf
+++ infra/azure/outputs.tf
@@ -9,3 +9,7 @@ output "aks_cluster_nodes" {
 output "acr_repo_name" {
   value = azurerm_container_registry.acr.name
 }
+
+output "Change_Config_Command" {
+  value = "az aks get-credentials --resource-group ${azurerm_resource_group.demo.name} --name ${azurerm_kubernetes_cluster.demo.name}"
+}
