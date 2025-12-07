c976a170496f1d3e4793fcd5d2a04bd07633d29e
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-07 16:37:27 +0530

    Normalization Lambda and Kinesis Source Mapping created

---
+++ infra/serverless/locals.tf
@@ -0,0 +1,22 @@
+# Capturing AWS Created Infrastructure from its bucket and using them in Serverless Stack
+data "terraform_remote_state" "aws" {
+  backend = "s3"
+  config = {
+    bucket = "aiops-platform-sf"
+    key    = "aws/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+locals {
+
+  kinesis_stream_arn = data.terraform_remote_state.aws.outputs.kinesis_stream_arn
+  kinesis_stream_name = data.terraform_remote_state.aws.outputs.kinesis_stream_name
+
+  common_tags = {
+    Project     = var.project_name
+    ManagedBy   = "terraform"
+    CreatedBy   = "DakshSawhney"
+    CreatedDate = formatdate("YYYY-MM-DD", timestamp())
+  }
+}
---
+++ infra/serverless/main.tf
@@ -0,0 +1,47 @@
+#########################
+# Bucket for storing data
+#########################
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  force_destroy = true
+
+  # aws s3 rm s3://aiopsplatform-data-bucket --recursive ## Delete bucket
+}
+
+########################
+# Normalization Lambda
+########################
+
+# Package the Lambda function code
+data "archive_file" "normalizer" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/normalizer.py"
+  output_path = "${path.module}/lambda/normalizer.zip"
+}
+
+resource "aws_lambda_function" "normalizer" {
+  filename         = data.archive_file.normalizer.output_path
+  function_name    = "normalizer-${var.project_name}"
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "normalizer.handler"
+  source_code_hash = data.archive_file.normalizer.output_base64sha256
+
+  runtime = "python3.12"
+
+  environment {
+    variables = {
+      TARGET_BUCKET = aws_s3_bucket.bucket.bucket
+    }
+  }
+
+  tags = local.common_tags
+}
+
+# Connect the kinesis steam with this lambda function
+resource "aws_lambda_event_source_mapping" "kinesis_trigger" {
+  event_source_arn = local.kinesis_stream_arn
+  function_name = aws_lambda_function.normalizer.arn
+  starting_position = "LATEST"    # Start processing new records only
+
+  batch_size = 100
+}
