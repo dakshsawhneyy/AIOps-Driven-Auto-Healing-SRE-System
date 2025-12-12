0b12156ce1dc7adbefcf568c754313ce4ad4f284
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-12 17:05:34 +0530

    Fetched dynamodb and sns from AWS Remote S3 Bucket

--- infra/serverless_inferencer/locals.tf
+++ infra/serverless_inferencer/locals.tf
@@ -10,6 +10,12 @@ data "terraform_remote_state" "aws" {
 
 locals {
 
+  sns_topic_arn = data.terraform_remote_state.aws.outputs.sns_topic_arn
+  sns_topic_name = data.terraform_remote_state.aws.outputs.sns_topic_name
+  dynamodb_table_arn = data.terraform_remote_state.aws.outputs.dynamodb_table_arn
+  dynamodb_table_name = data.terraform_remote_state.aws.outputs.dynamodb_table_name
+  kinesis_stream_arn = data.terraform_remote_state.aws.outputs.normalizer_kinesis_stream_arn
+
   common_tags = {
     Project     = var.project_name
     ManagedBy   = "terraform"
