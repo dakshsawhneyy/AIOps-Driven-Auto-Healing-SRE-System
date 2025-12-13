54fb8c14a65cbb4a829989c5e9eea9764bd63cfe
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-14 01:00:29 +0530

    Auto Healer getting requests from Lambda through secure Header

--- infra/azure/AUTO_HEALER/Auto-Healer-App/autoheal-controller.py
+++ infra/azure/AUTO_HEALER/Auto-Healer-App/autoheal-controller.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException, Depends
 from kubernetes import client, config
+import os
 
 app = FastAPI()
 
@@ -8,7 +9,15 @@ config.load_incluster_config()
 k8s = client.AppsV1Api()
 core = client.CoreV1Api()
 
-@app.post("/heal/cpu")
+API_KEY = os.environ["API_KEY"]
+
+# Send a correct api key along with request
+def auth(x_api_key: str = Header(None)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+@app.post("/heal/cpu", dependencies=[Depends(auth)])
 def scale_backend():
     k8s.patch_namespaced_deployment_scale(
         name="backend-deployment",
@@ -17,7 +26,8 @@ def scale_backend():
     )
     return {"status": "scaled backend to 4 replicas"}
 
-@app.post("/heal/memory")
+
+@app.post("/heal/memory", dependencies=[Depends(auth)])
 def restart_pod():
     pods = core.list_namespaced_pod("default", label_selector="app=backend").items
     if pods:
@@ -25,7 +35,8 @@ def restart_pod():
         return {"status": f"restarted pod {pods[0].metadata.name}"}
     return {"status": "no pod found"}
 
-@app.post("/heal/traffic")
+
+@app.post("/heal/traffic", dependencies=[Depends(auth)])
 def scale_frontend():
     k8s.patch_namespaced_deployment_scale(
         name="frontend-deployment",
