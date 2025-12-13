28e6c1d6ea75ff5775ae18b3ccfd2eddacf3109a
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-14 01:10:08 +0530

    Roles and IAMs built for auto scaling engine

---
+++ infra/auto_heal_engine/security.tf
@@ -0,0 +1,57 @@
+# Creation of IAM Role
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "${var.project_name}-auto-healer-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Attatching policy with ECS Role
+resource "aws_iam_role_policy_attachment" "lambda_execution_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "auto_healer_ddb" {
+  role = aws_iam_role.lambda_execution_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem"
+        ]
+        Resource = local.dynamodb_table_arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Resource = "arn:aws:secretsmanager:ap-south-1:897722695334:secret:AIOps-Platform-Secrets-78qeGk"
+      },
+    ]
+  })
+}
+
+
+# Security Group for Lambda
+resource "aws_security_group" "lambda_sg" {
+  vpc_id = module.vpc.vpc_id
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
